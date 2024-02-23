623822382b0614346b098b926b1c4e52e0568819
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2024-02-23 23:42:10 +0600

    Update this code

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from coreapp import views
 urlpatterns = [
     path('home/',views.home, name= 'homepage'),
+    path('login/',views.user_login, name= 'user_login'),
+    path('register/',views.user_register, name= 'user_register'),
 ]
--- coreapp/views.py
+++ coreapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+def user_login(request):
+    return render(request, 'home/user_login.html')
+
+def user_register(request):
+    return render(request, 'home/user_register.html')
