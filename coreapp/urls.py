4ec2dae238060e4711d74816622b66659e53d720
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2024-02-26 00:45:17 +0600

    This Project work Login section

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('home/',views.home, name= 'homepage'),
     path('login/',views.user_login, name= 'user_login'),
     path('register/',views.user_register, name= 'user_register'),
+    path('logout/',views.user_logout, name= 'user_logout'),
 ]
--- coreapp/views.py
+++ coreapp/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 # Create your views here.
 
 def home(request):
     return render(request, 'home/home.html')
 
-def user_login(request): 
-    
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']           
+        user_name = authenticate(username=username)
+        user_pass = authenticate(password=password)
+        if user_name is None:
+            messages.error(request, 'User Not Found')
+            return redirect(user_login)
+        else:
+            login(request,user_name)
+            return redirect(home)
+        
+        
     return render(request, 'home/user_login.html')
-
+    
 # Register Base Function
 def user_register(request):
     if request.method == 'POST':
@@ -17,18 +30,13 @@ def user_register(request):
         first_name = request.POST['first_name']
         email = request.POST['email']
         password = request.POST['password']
-        con_password = request.POST['con_password']
-        
+        con_password = request.POST['con_password'] 
         # chack in password1 or password2 similar 
         if password == con_password:
             if User.objects.filter(username=username).exists():
                 messages.error(request, 'Oops! Username is already taken. Please choose another one.')
-                return redirect(user_register)
-            if User.objects.filter(email=email):
-                messages.error(request, 'Oops! Email is already registered. Please use a different email.')
-                return redirect(user_register)
-                  
-            # Field is not Empty 
+                return redirect(user_register)              
+            # Field is not Empty
             if username and email and password and con_password:
                 user_info=User.objects.create_user(username=username, email=email, first_name=first_name, password=password)
                 user_info.save()
@@ -38,3 +46,7 @@ def user_register(request):
         else:
             messages.error(request, 'Oops! Passwords do not match. Please check and try again.')
     return render(request, 'home/user_register.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect(home)
